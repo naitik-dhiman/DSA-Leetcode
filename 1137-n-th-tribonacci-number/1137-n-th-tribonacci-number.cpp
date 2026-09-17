2eda40f5e4744bd842f5bf56767191ba697c750b
naitik-dhiman <naitikdhiman284@gmail.com>
2026-09-17 10:51:26 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.39%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1; 
+        
+        int a = 0;
+        int b = 1;
+        int c = 1; 
+         
+        for (int i = 3; i <= n; i++){
+            int next_val = a + b + c;
+            a = b;
+            b = c;
+            c = next_val;
+        }
+        return c; 
+    }
+};
